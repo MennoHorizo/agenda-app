d031d4560946641b037123d8f80f48a1f398b15f
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-11-25 15:10:53 +0100

    Added app css file

--- resources/views/layouts/standard-page-layout.blade.php
+++ resources/views/layouts/standard-page-layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <title>Document</title>
