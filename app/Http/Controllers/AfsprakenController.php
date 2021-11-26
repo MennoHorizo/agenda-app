0acdf550895d92682a5dcfe2f145cc8f43fd7800
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-11-26 11:34:12 +0100

    Afsprakencontroller aangepast

--- app/Http/Controllers/AfsprakenController.php
+++ app/Http/Controllers/AfsprakenController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AfsprakenController extends Controller
 {
+    //constructor
+    public function __construct(){
+        //stel de middelware in dat alleen authenticated users toegang tot de pagina hebben.
+        $this->middleware("auth");
+    }
+
     //
+    public function loadCalendar(){
+        return 123;
+    }
+
 }
--- resources/views/layouts/standard-page-layout.blade.php
+++ resources/views/layouts/standard-page-layout.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    <title>Agenda</title>
+    <title>Awesome-O</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
