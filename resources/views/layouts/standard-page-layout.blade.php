e8c80daedf3a250ee58f740331016c533f5d1a8d
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-11-26 10:10:04 +0100

    pauze

--- resources/views/layouts/standard-page-layout.blade.php
+++ resources/views/layouts/standard-page-layout.blade.php
@@ -9,7 +9,9 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-    <title>Document</title>
+    <title>Agenda</title>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
     <div class="flex-center position-ref full-height">
