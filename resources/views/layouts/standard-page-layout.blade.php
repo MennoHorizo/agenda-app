dd10d2fc49e245c1d0a640f1681dc6bb38a277e8
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-11-26 09:38:42 +0100

    csrf token geadd aan de standaard page layout

--- resources/views/layouts/standard-page-layout.blade.php
+++ resources/views/layouts/standard-page-layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{asset('css/app.css')}}" />
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}" />
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
